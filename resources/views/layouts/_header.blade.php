4a9e0a3caa6d0cde2e4484e918d15e498859bdbe
git-git-git1 <3203117280@qq.com>
2020-02-07 10:31:40 +0000

    注册页面

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container ">
     <a class="navbar-brand" href="/">Weibo App</a>
-    <ul class="navbar-nav justify-content-end">
+    <ul class="nav nav-tabs">
       <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">帮助</a></li>
       <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
     </ul>
