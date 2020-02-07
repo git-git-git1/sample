0e68bea0f4219da20515a02c7178cd9561e5bcbc
git-git-git1 <3203117280@qq.com>
2020-02-07 07:58:38 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,26 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>@yield('title', 'Sample') - Laravel 新手入门教程</title>
-</head>
-<body>
-  @yield('content');
-</body>
+  <head>
+    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+    <link rel="stylesheet"  href="bootstrap/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/app.css">
+    <script src="bootstrap/js/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="/">Weibo App</a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="navbar-link" href="/help">帮助</a></li>
+          <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      @yield('content')
+    </div>
+  </body>
 </html>
