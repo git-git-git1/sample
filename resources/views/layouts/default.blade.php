764236e8d42ded460816915f539638611dc0271d
git-git-git1 <3203117280@qq.com>
2020-02-10 07:45:23 +0000

    Finish login and logout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,8 +4,8 @@
     <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
     <link rel="stylesheet"  href="/bootstrap/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="/css/app.css">
-    <script src="/bootstrap/js/jquery.min.js"></script>
-    <script src="/bootstrap/js/bootstrap.min.js"></script>
+    <!-- <script src="/bootstrap/js/jquery.min.js"></script>
+    <script src="/bootstrap/js/bootstrap.min.js"></script> -->
   </head>
   <body>
 
@@ -18,5 +18,8 @@
       </div>
     </div>
      @include('layouts._footer')
+
+
+     <script src="/js/app.js"></script>
   </body>
 </html>
