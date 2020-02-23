9955ca1b86956693ad18f9f9054a8e34972494a3
git-git-git1 <3203117280@qq.com>
2020-02-23 08:40:39 +0000

    Add user following

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,12 @@ Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
+//关注人列表
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+//粉丝列表
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
+//关注用户
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+//取消用户
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
